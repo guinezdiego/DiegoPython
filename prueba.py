565e487e7711c96ddbb2ac4e92e2b41e8c9577a3
guinezdiego <guinezdiego3103@gmail.com>
2026-04-17 10:12:40 -0400

    primer commit

---
+++ prueba.py
@@ -0,0 +1,9 @@
+credito = 0
+ingresos = int(input("Ingrese cantidad de ingresos: "))
+educacion = input("Ingrese nivel educacional: ")
+nacionalidad = input("Ingrese nacionalidad: ")
+
+if ingresos >= 500000 and ingresos <= 1000000:
+    print("vas por buen camino")
+else:
+    print("pobre")
