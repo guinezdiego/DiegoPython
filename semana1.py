00a068c928a153d6816cd5705a8d7bfbb9b60d7d
guinezdiego <guinezdiego3103gmail.com>
2026-05-13 00:46:06 -0400

    iniciando

---
+++ semana1.py
@@ -0,0 +1,104 @@
+# # Ejercicio 1
+
+# nombre = input("Ingrese su nombre: ")
+# edad = int(input("Ingrese su edad: "))
+# carrera = input("Ingrese su carrera: ")
+
+# print(f"Hola {nombre}")
+# print(f"Tienes {edad} años")
+# print(f"Estudias {carrera}")
+
+# # Ejercicio 2
+
+# n1 = int(input("Ingrese un numero: "))
+# n2 = int(input("Ingrese otro numero: "))
+
+# while n2 == 0:
+#     print("No se puede dividir por 0")
+#     n2 = int(input("Ingrese otro numero: "))
+
+# print(f"La suma de los numeros es: {n1 + n2}")
+# print(f"La resta de los numeros es: {n1 - n2}")
+# print(f"EL total del la multiplicacion es: {n1 * n2}")
+# print(f"El total de la division es: {n1 / n2}")
+
+# # Ejercicio 3
+
+# notas = int(input("Ingrese el numero de notas: "))
+# suma_total = 0
+
+# for nota in range(notas):
+#     nota = float(input("Ingrese su nota: "))
+#     suma_total += nota
+
+# promedio = suma_total / notas
+
+# print(f"El promedio final es de: {promedio}")
+
+# # Ejercicio 4
+
+# celsius = int(input("Ingrese los grados C°: "))
+# fahren = celsius * (9 / 5) + 32
+
+# print(f"Grados Fahrenheit: {fahren}")
+
+# # Ejercicio 5
+
+# num = int(input("Ingrese un numero: "))
+
+# if num % 2 == 0:
+#     print("El numero es par")
+# else:
+#     print("El numero es impar")
+
+# # Mini desafio
+
+# nombre = input("Ingrese su nombre: ")
+# notas = int(input("Ingrese el numero de notas: "))
+# suma_notas = 0
+
+# for i in range(notas):
+#     nota = float(input("Ingrese su nota: "))
+#     suma_notas += nota
+
+# promedio = suma_notas / notas
+
+# if promedio >= 4:
+#     print(f"{nombre} tu promedio final es: {promedio} estas aprobado")
+# else:
+#     print(f"{nombre} tu promedio final es: {promedio} estas reprobado")
+
+# # Mini prueba
+# # Pregunta 1
+# base = int(input("Ingrese la base: "))
+# altura = int(input("Ingrese la altura: "))
+# area = base * altura
+# print(f"El area total es de: {area}")
+
+# # Pregunta 2
+
+# num = int(input("Ingrese un numero: "))
+# print(f"El doble de {num} es: {num * 2} y el triple de {num} es: {num * 3}")
+
+# # Pregunta 3
+
+# segundos = int(input("Ingrese los segundos: "))
+# minutos = segundos // 60
+
+# print(f"El total de minutos es: {minutos}")
+
+# # Reto final del dia
+
+# num1 = int(input("Ingrese un numero: "))
+# num2 = int(input("Ingrese otro numero: "))
+
+# while num2 == 0:
+#     print("No se puede dividir por 0")
+#     num2 = int(input("Ingrese otro numero: "))
+    
+# print(f"El total de la suma es: {num1 + num2}")
+# print(f"El total de la resta es: {num1 - num2}")
+# print(f"El total de la multiplicacion es: {num1 * num2}")
+# print(f"El total de la division es: {num1 / num2}")
+# print(f"El total de la potencia es: {num1 ** num2}")
+
